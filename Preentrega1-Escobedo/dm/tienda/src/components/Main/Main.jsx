6b467d2472da785fab6df4790d2031c9ef0810fc
Pedro <113485042+Pescobedo1982@users.noreply.github.com>
2023-02-14 22:15:53 -0300

    Add files via upload
    probando

---
+++ Preentrega1-Escobedo/dm/tienda/src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ItemListContainer } from "./ItemListContainer";
+
+export const Main = () => {
+  return (
+    <><ItemListContainer greeting = {"PAGINA EN CONSTRUCCIÓN."}/></>
+  )
+}
+
+export default Main
