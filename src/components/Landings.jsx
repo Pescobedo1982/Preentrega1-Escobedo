b3ba96f1b6023de3ff1e43249f4d5d53e89850c5
Pedro <113485042+Pescobedo1982@users.noreply.github.com>
2023-02-14 21:43:58 -0300

    Add files via upload

---
+++ src/components/Landings.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar/Navbar.jsx';
+import Main from "./Main/Main.jsx";
+function Landings() {
+    return (
+        <>
+        <Navbar />
+        <Main/>
+        </>
+    );
+}
+
+export default Landings;
---
+++ src/components/Main/ItemListContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const ItemListContainer = ({greeting}) => {
+
+  return (
+    <div><p className='mdP'>{greeting}</p></div>
+  )
+}
